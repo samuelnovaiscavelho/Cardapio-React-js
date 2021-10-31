8ca9b0bbcd8a67d5c23ae4ccb726c59edf7927da
samuelnovaiscavelho <rm88233@fiap.com.br>
2021-10-31 03:56:08 -0300

    commit 7

--- src/pages/Services/Data.js
+++ src/pages/Services/Data.js
@@ -39,3 +39,18 @@ export const homeObjOne = {
     img: require('../../images/restau.jpg'),
     alt: 'Vault'
   };
+  
+  export const homeObjFour = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Data Analytics',
+    headline: 'Every transaction is stored on our secure cloud database',
+    description:
+      'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
+    buttonLabel: 'Sign Up Now',
+    imgStart: 'start',
+    img: require('../../images/svg-1.svg'),
+    alt: 'Vault'
+  };
+  
---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TodoList from '../../components/TodoList.js'
+
+import './Lista.css'
+
+
+function SignUp() {
+  return (
+    <div className='todo-app'>
+
+     <TodoList/>
+    
+
+    </div>
+  );
+}
+
+export default SignUp;
