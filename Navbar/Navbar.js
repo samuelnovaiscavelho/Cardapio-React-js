d6a66e11adb0aa57977d67f7001121c094646cd0
samuelnovaiscavelho <rm88233@fiap.com.br>
2021-10-31 12:25:34 -0300

    commit 17

--- Navbar/Navbar.js
+++ Navbar/Navbar.js
@@ -14,3 +14,75 @@ import {
   NavLinks,
   NavBtnLink
 } from './Navbar.elements';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to='/' onClick={closeMobileMenu}>
+              <NavIcon />
+              CARDAPIO
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks to='/' onClick={closeMobileMenu}>
+                  Início
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to='/services' onClick={closeMobileMenu}>
+                  Serviços
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to='/products' onClick={closeMobileMenu}>
+                  Cardapio
+                </NavLinks>
+              </NavItem>
+              <NavItemBtn>
+                {button ? (
+                  <NavBtnLink to='/sign-up'>
+                    <Button primary>Acompanhe Seu Pedido</Button>
+                  </NavBtnLink>
+                ) : (
+                  <NavBtnLink to='/sign-up'>
+                    <Button onClick={closeMobileMenu} fontBig primary>
+                      SIGN UP
+                    </Button>
+                  </NavBtnLink>
+                )}
+              </NavItemBtn>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
