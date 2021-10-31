81fdb1b056e0d61d93981abde7fd2b0efce3d5d4
samuelnovaiscavelho <rm88233@fiap.com.br>
2021-10-31 03:09:42 -0300

    commit 6

---
+++ Services/Data.js
@@ -0,0 +1,41 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Temos variações de Pratos, mas prezamos pela qualidade e pela entrega ! ',
+    headline: 'Segue nossos pratos',
+    description:
+      'É preciso conhecer as diferentes tipologias de restaurantes. Afinal, um restaurante não é apenas um estabelecimento onde as pessoas vão comer: é um local que deve também encantar o cliente por seu estilo, sua proposta e seu atendimento.',
+    buttonLabel: 'Acompanhe Seu Pedido',
+    imgStart: '',
+    img: require('../../images/pratos.jpg'),
+    alt: 'Credit Card'
+  };
+  
+  export const homeObjTwo = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '100% Secure',
+    headline: 'Stay protected 24/7 anywhere anytime',
+    description:
+      'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime',
+    buttonLabel: 'Learn More',
+    imgStart: '',
+    img: require('../../images/svg-1.svg'),
+    alt: 'Vault'
+  };
+  
+  export const homeObjThree = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: '',
+    headline: 'A limpeza e agilidade é algo fundamental para os nossos clientes',
+    description:
+      "Nosso funcionamento é das 09:00 ás 18:00, venha conhecer nosso restaurante e fazer parte do time chamado Cardapio.",
+    buttonLabel: 'Acompanhe Seu Pedido',
+    imgStart: 'start',
+    img: require('../../images/restau.jpg'),
+    alt: 'Vault'
+  };
--- src/pages/Products/Data.js
+++ src/pages/Products/Data.js
@@ -27,3 +27,32 @@ export const homeObjOne = {
     img: require('../../images/pratosDoDia.jpg'),
     alt: 'Vault'
   };
+  
+  export const homeObjThree = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Easy Setup',
+    headline: 'Super fast and simple onboarding process',
+    description:
+      "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+    buttonLabel: 'Start Now',
+    imgStart: 'start',
+    img: require('../../images/svg-1.svg'),
+    alt: 'Vault'
+  };
+  
+  export const homeObjFour = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'DATA ANALYTICS',
+    headline: 'Every transaction is stored on our secure cloud database',
+    description:
+      'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
+    buttonLabel: 'Sign Up Now',
+    imgStart: 'start',
+    img: require('../../images/svg-1.svg'),
+    alt: 'Vault'
+  };
+  
