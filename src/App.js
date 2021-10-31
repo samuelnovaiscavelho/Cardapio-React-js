b709c906d2260dcda641a0959b6b344a64730b24
samuelnovaiscavelho <rm88233@fiap.com.br>
2021-10-31 02:48:18 -0300

    terceiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import ScrollToTop from './components/ScrollToTop';
+import { Navbar, Footer } from './components';
+
+import TodoList from './components/TodoList'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <ScrollToTop />
+      <Navbar />
+    
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/sign-up'component={SignUp}/>
+        
+       
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
