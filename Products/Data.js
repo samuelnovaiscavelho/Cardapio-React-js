06b1acf7dcdabea865911159dbb0e9bdf5dcf635
samuelnovaiscavelho <rm88233@fiap.com.br>
2021-10-31 03:03:57 -0300

    commit 5

---
+++ Products/Data.js
@@ -0,0 +1,29 @@
+export const homeObjOne = {
+    lightBg: true,
+    lightText: false,
+    lightTopLine: false,
+    lightTextDesc: false,
+    topLine: 'Segue o Cardápio do Dia',
+    headline: 'Consulte sua alimentação em nosso cardápio.',
+    description:
+      'A saúde é algo fundamental para o ser humano com isso prezamos alimentos de qualidade para todos, sendo nossos clientes e colaboradores da nossa organização.',
+    buttonLabel: 'Acompanhe seu Pedido',
+    imgStart: '',
+    img: require('../../images/cardapio.jpg'),
+    alt: 'Credit Card'
+  };
+  
+  export const homeObjTwo = {
+    lightBg: false,
+    lightText: true,
+    lightTopLine: true,
+    lightTextDesc: true,
+    topLine: '100% de qualidade em nossos pratos',
+    headline: 'Venha experimentar os nossos pratos',
+    description:
+      'Temos Salmão, Pernil e Frago com acompanhamentos para suprir sua fome.',
+    buttonLabel: 'Acompanhe Seu Pedido',
+    imgStart: '',
+    img: require('../../images/pratosDoDia.jpg'),
+    alt: 'Vault'
+  };
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,71 @@
+export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Bem-vindo ao mundo das delícias',
+    headline: 'Conheça o nosso cardapio do dia e aproveite as delicias disponíveis.',
+    description:
+      'Buscamos sempre oferecer os pratos mais deliciosos e saudáveis para nossos clientes !',
+    buttonLabel: 'Faça Seu Pedido',
+    imgStart: '',
+    img: require('../../images/comer.gif'),
+    alt: 'Credit Card',
+    start: ''
+  };
+  
+  export const homeObjTwo = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Temos pratos deliciosos, aproveite',
+    headline: 'Dê uma olhada nos nossos valores acessíveis',
+    description:
+      "Fique de olho nos dias da semana, pois cada dia especifico optamos oferecer um prato especifico para os nosso clientes e colaboradores",
+    buttonLabel: 'Faça Seu Pedido ',
+    imgStart: '',
+    img: require('../../images/restaunt.jpg'),
+    alt: 'Vault',
+    start: ''
+  };
+  
+  export const homeObjThree = {
+    primary: false,
+    lightBg: true,
+    lightTopLine: false,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Equipe HelloDevs', 
+    headline:
+      'Somos os responsáveis para matar o seu apetite !',
+      description:
+  
+      "☻ Érlis Brito Gonçalves de Lima, ♣ Felipe Gustavo Alves Gonçalves, ♥ Glória Maria Ribeiro Souza, ◘ Leonardo Cezar de Oliveira, ♦ Pedro Marques Alencar Leite, ◙ Samuel Novais Cavelho.",
+  
+    buttonLabel: 'Faça Seu Pedido',
+    imgStart: 'start',
+    img: require('../../images/profile.jpg'),
+    alt: 'Vault',
+    start: 'true'
+  };
+  
+  export const homeObjFour = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Dica do dia - Modo de Preparo ☺',
+    headline: 'OMELETE',
+    description:
+      '2 ovos, 1 pitada de sal, 1 fatia de presunto, 2 fatias de queijo, tempero verde a gosto e caldo de galinha a gosto. Bata os 2 ovos, pode ser na batedeira ou não. Bata os 2 ovos, pode ser na batedeira ou não, após ter batido bem, coloque-o na frigideira já untada com óleo, acrescente o sal, o presunto picado em quadradinhos e as duas fatias de queijo (não precisa picar o queijo). Coloque os temperos a gosto, espere ficar firme, e vire o omelete, está pronto um omelete delicioso. Bom apetite!',
+    buttonLabel: 'Faça seu Pedido',
+    imgStart: 'start',
+    img: require('../../images/omelete.jpeg'),
+    alt: 'Vault',
+    start: 'true'
+  };
+  
